68a153ee01433b1ddfad886366260c8687d4a232
DESKEDIT <harryroberts306@gmail.com>
2025-09-28 19:07:44 -0400

    Add b0.2.0

---
+++ version/beta/0.2.0/vars.hpp
@@ -0,0 +1,9 @@
+#include <string>
+
+#define K_PRETYPE (std::string)"Beta"
+#define K_VERSION (std::string)"0.2"
+#define K_YEAR (std::string)"2025"
+#define K_MON (std::string)"9"
+#define K_DAY (std::string)"28"
+
+#define K_STARTUP "Kernel ++ " + K_PRETYPE + " " + K_VERSION + " of " + K_YEAR + "." + K_MON + "." + K_DAY
