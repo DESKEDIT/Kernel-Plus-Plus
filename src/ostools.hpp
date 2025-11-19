add6fa8d3514ea390760fb4077c42b759ebf4d3c
DESKEDIT <harryroberts306@gmail.com>
2025-11-19 07:55:05 -0500

    Fix compilation error when using Windows

--- src/ostools.hpp
+++ src/ostools.hpp
@@ -15,7 +15,7 @@ void ClearScreen()
 #if defined(__unix__) || defined(__unix) || defined(__linux__) || (defined(__APPLE__) && defined(__MACH__))
   system("clear");
 #elif defined(_WIN32) || defined(_WIN64)
-  system("cls")
+  system("cls");
 #endif
 }
 
