39424586c670bc009af6c408f2fa23709643b534
Harry Roberts <harry@S7Chronos>
2025-11-18 19:51:01 -0500

    Clear screen when executing SysErr function

---
+++ build/CMakeFiles/Kernel.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Kernel.dir/link.d"
+  "CMakeFiles/Kernel.dir/src/main.cpp.o"
+  "CMakeFiles/Kernel.dir/src/main.cpp.o.d"
+  "Kernel"
+  "Kernel.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Kernel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
