b4a89463cddb90f34d12beb5de09a75bfb4beec2
Harry Roberts <harry@S7Chronos>
2025-11-18 19:46:13 -0500

    Add OS tools header with (bool IsOSCompatible()) and (void ClearScreen()) functions

---
+++ src/ostools.hpp
@@ -0,0 +1,24 @@
+#ifndef OS_TOOLS
+
+#include <cstdlib>
+
+bool IsOSCompatible()
+{
+#if defined(__unix__) || defined(__unix) || defined(__linux__) || (defined(__APPLE__) && defined(__MACH__)) || defined(_WIN32) || defined(_WIN64) 
+  return true;
+#endif
+  return false;
+}
+
+void ClearScreen()
+{
+#if defined(__unix__) || defined(__unix) || defined(__linux__) || (defined(__APPLE__) && defined(__MACH__))
+  system("clear");
+#elif defined(_WIN32) || defined(_WIN64)
+  system("cls")
+#endif
+}
+
+#define OS_TOOLS
+#endifdefined(__unix__) || defined(__unix) || defined(__linux__) || (defined(__APPLE__) && defined(__MACH__))
+
