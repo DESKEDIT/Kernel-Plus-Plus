a5c84f578da497c71e651606281fbbb16d91c76c
DESKEDIT <135285142+DESKEDIT@users.noreply.github.com>
2026-01-01 18:30:13 -0500

    Update version and date macros in m.hpp  because i forgot to when i released 0.2.1

--- src/m.hpp
+++ src/m.hpp
@@ -1,9 +1,9 @@
 #ifndef MACRO_DEFAULT
 #define K_PRETYPE (std::string)"Beta"
-#define K_VERSION (std::string)"0.2.1-dev"
-#define K_DAY (std::string)"18"
-#define K_MON (std::string)"11"
-#define K_YEAR (std::string)"2025"
+#define K_VERSION (std::string)"0.2.1"
+#define K_DAY (std::string)"01"
+#define K_MON (std::string)"01"
+#define K_YEAR (std::string)"2026"
 
 #define K_STARTUP "Kernel ++ " + K_PRETYPE + " " + K_VERSION + " of " + K_YEAR + "." + K_MON + "." + K_DAY
 #define MACRO_DEFAULT
